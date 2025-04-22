dea4707c294d285eba85e278d19613f8222a7199
JesusCiber <fjesuscl@gmail.com>
2025-04-22 16:35:15 +0200

    Class book

--- src/main/java/com/grupo5/homework_java_ironlibrary/models/Book.java
+++ src/main/java/com/grupo5/homework_java_ironlibrary/models/Book.java
@@ -1,4 +1,69 @@
 package com.grupo5.homework_java_ironlibrary.models;
 
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table (name = "book")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private String isbn;
+    private String title;
+    private String category;
+    private int quantity;
+
+    public  Book(){
+    }
+
+    public Book(String isbn, String title, String category, int quantity) {
+        this.isbn = isbn;
+        this.title = title;
+        this.category = category;
+        this.quantity = quantity;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", category='" + category + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
