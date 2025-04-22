0d366607fd1906e1067effa70ac4a26fd3cf9065
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2025-04-22 15:11:53 +0200

    Initial commit

---
+++ src/main/java/com/grupo5/homework_java_ironlibrary/HomeworkJavaIronlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.grupo5.homework_java_ironlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkJavaIronlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkJavaIronlibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo5/homework_java_ironlibrary/HomeworkJavaIronlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo5.homework_java_ironlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkJavaIronlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
