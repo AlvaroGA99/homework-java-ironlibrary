e0457088c8cebcdf25ec892fcb3b601f5eb3e5b6
JesusCiber <fjesuscl@gmail.com>
2025-04-23 19:44:44 +0200

    addBook

--- src/main/java/com/grupo5/homework_java_ironlibrary/models/LibraryHandler.java
+++ src/main/java/com/grupo5/homework_java_ironlibrary/models/LibraryHandler.java
@@ -4,8 +4,8 @@ import com.grupo5.homework_java_ironlibrary.repositories.AuthorRepository;
 import com.grupo5.homework_java_ironlibrary.repositories.BookRepository;
 import com.grupo5.homework_java_ironlibrary.repositories.IssueRepository;
 import com.grupo5.homework_java_ironlibrary.repositories.StudentRepository;
-import com.grupo5.homework_java_ironlibrary.repositories.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Scanner;
 
 
 public class LibraryHandler {
@@ -25,6 +25,47 @@ public class LibraryHandler {
 
     public void findStudentById( String id) {
         System.out.println(studentRepository.findById(id));
+    }
+
+    public void addBook(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter isbn: ");
+        String isbn = scanner.nextLine();
+
+        System.out.print("Enter title: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Enter category: ");
+        String category = scanner.nextLine();
+
+        System.out.print("Enter Author name: ");
+        String authorName = scanner.nextLine();
+
+        System.out.print("Enter Author mail: ");
+        String authorEmail = scanner.nextLine();
+
+        System.out.print("Enter number of books: ");
+        int quantity = Integer.parseInt(scanner.nextLine());
+
+        Book book = new Book();
+        book.setIsbn(isbn);
+        book.setTitle(title);
+        book.setCategory(category);
+        book.setQuantity(quantity);
+
+        Author author = new Author();
+        author.setName(authorName);
+        author.setEmail(authorEmail);
+        author.setAuthorBook(book);
+
+        book.setAuthor(author);
+        // Guardar en orden correcto
+        bookRepository.save(book);
+        //authorRepository.save(author);
+
+
+        System.out.println("Book and author saved!");
     }
 
 }
