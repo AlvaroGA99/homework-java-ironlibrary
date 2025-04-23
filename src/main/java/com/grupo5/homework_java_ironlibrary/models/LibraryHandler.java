7c54f6951918cad5091dac2b15209b11f3328a47
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2025-04-23 13:20:28 +0200

    PUll from main and library handler

--- src/main/java/com/grupo5/homework_java_ironlibrary/models/LibraryHandler.java
+++ src/main/java/com/grupo5/homework_java_ironlibrary/models/LibraryHandler.java
@@ -1,4 +1,14 @@
 package com.grupo5.homework_java_ironlibrary.models;
 
+import com.grupo5.homework_java_ironlibrary.HomeworkJavaIronlibraryApplication;
+import com.grupo5.homework_java_ironlibrary.repositories.AuthorRepository;
+import com.grupo5.homework_java_ironlibrary.repositories.BookRepository;
+import com.grupo5.homework_java_ironlibrary.repositories.IssueRepository;
+import com.grupo5.homework_java_ironlibrary.repositories.StudentRepository;
+
 public class LibraryHandler {
+    private static AuthorRepository authorRepository;
+    private static BookRepository bookRepository;
+    private static IssueRepository issueRepository;
+    private static StudentRepository studentRepository;
 }
